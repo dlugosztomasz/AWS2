c475e78eb12c3888c71e636afbfbeecdafe77507
tomasz-dlugosz <tomasz.dlugosz@schibsted.com>
2022-12-07 12:49:00 +0100

    feat: easy structure

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
+const app = express()
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+  res.send("<html><div style='text-align:center;margin-top:20%'><h1>Main Page</h1></div></html>");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const port = process.env.port || '8080';
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const httpServer = http.createServer(app);
+httpServer.listen(port, () => {
+  console.log(`API is now live on ${port}`);
 });
-
-module.exports = app;
